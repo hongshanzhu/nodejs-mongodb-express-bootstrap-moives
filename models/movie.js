6b1424fcb352675f93e8e6e82cc23a3f4a7ccf55
hongshanzhu <hongshan.zhu@ericsson.com>
2017-01-21 17:03:16 +0800

    moives

--- models/movie.js
+++ models/movie.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose')
+
 var MovieSchema = require('../schemas/movie')
 //Ä£ÐÍ
 var Movie = mongoose.model('Movie',MovieSchema)
 
-
-module.exports  = Movie 
+module.exports = Movie
